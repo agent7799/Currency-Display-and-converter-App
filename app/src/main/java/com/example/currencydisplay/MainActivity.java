6dda5693b54ccc0ce89685d4dcd376d3dda41f8c
Agent7799 <agent7799@mail.ru>
2022-03-13 01:22:50 +0300

    Currency Display App: createValuteList method added

--- app/src/main/java/com/example/currencydisplay/MainActivity.java
+++ app/src/main/java/com/example/currencydisplay/MainActivity.java
@@ -12,9 +12,11 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import java.io.*;
 import java.net.*;
+import java.sql.Time;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.TreeMap;
+import java.util.List;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG_NOMINAL = "Nominal";
     private static final String TAG_VALUTE = "Valute";
     private final String currencies = "https://www.cbr-xml-daily.ru/daily_json.js";
+    private static Date date;
+    private static Date previousDate;
+    private static URL previousURL;
+    private static Time timestamp;
 
     {
         try {
@@ -49,6 +55,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 getWeb();
+
             }
         };
         secThread = new Thread(runnable);
@@ -56,21 +63,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getWeb() {
+        JsonArray jsonArray = new JsonArray();
 
         try (InputStream in =  link.openStream()){
-            JsonElement root = parseReader(new BufferedReader(new InputStreamReader(in)));//Convert the input stream to a json element
+//Convert the input stream to a json element
+            JsonElement root = parseReader(new BufferedReader(new InputStreamReader(in)));
             JsonObject jsonObject = root.getAsJsonObject(); //May be an array, may be an object.
-
 //string from valute JSON
             //String val = jsonObject.get(TAG_VALUTE).toString();
-
             JsonObject valuteJson = jsonObject.getAsJsonObject(TAG_VALUTE);
-            Log.d("MyLog", "Valute js: " + valuteJson.toString());
+
+            Log.d("MyLog", "array js: " + valuteJson.isJsonArray());
 
             String val = jsonObject.get(TAG_VALUTE).toString().substring(1, jsonObject.get(TAG_VALUTE).toString().length() - 1);
             //Log.d("MyLog", "Valute string: " + val);
             String[] res = val.split("\\},");
-            JsonArray jsonArray = new JsonArray();
+
 
 //add  JSON objects to JSON array  from strings
             for (int i = 0; i < res.length; i++) {
@@ -83,34 +91,34 @@ public class MainActivity extends AppCompatActivity {
                 jsonArray.add(parseString(res[i]).getAsJsonObject());
             }
 
-            Map<String, Long> currencyMap = new TreeMap<String, Long>();
-//add currency name and val to map
-            String currencyName;
-            Long currencyValue;
-            for (int i = 0; i < jsonArray.size(); i++) {
-                 //Log.d("MyLog","i: " + i + ": " +  jsonArray.get(i));
-                 //Log.d("MyLog","i: " + i + ": " +  ((JsonObject)jsonArray.get(i)).get(TAG_NAME));
-
-                currencyMap.put(
-                        ((JsonObject)jsonArray.get(i)).get(TAG_NAME).toString(),
-                        ((JsonObject)jsonArray.get(i)).get(TAG_RATE).getAsLong()
-                );
-            }
-
-//get every value from array
-            Iterator i = jsonArray.iterator();
-            while (i.hasNext()) {
-                 JsonObject innerObj = (JsonObject) i.next();
-
-
-                 Log.d("MyLog", innerObj.get(TAG_RATE) + " рублей за " + innerObj.get(TAG_NOMINAL) + " " + innerObj.get(TAG_NAME));
-            }
-
+            createValuteList(jsonArray);
+            Log.d("MyLog","res string: " + createValuteList(jsonArray).size() + " vallutes created");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+
+
+private List<Valute> createValuteList(JsonArray jsonArray){
+        List<Valute> valuteList = new ArrayList<>();
+    //get every value from array
+    Iterator i = jsonArray.iterator();
+    while (i.hasNext()) {
+        JsonObject obj = (JsonObject) i.next();
+        valuteList.add(new Valute(
+                obj.get("Nominal").getAsInt(),
+                obj.get("Name").getAsString(),
+                (long) (obj.get("Value").getAsDouble()*100)));
+    }
+
+    for (int j = 0; j < valuteList.size(); j++) {
+        Log.d("MyLog", "createValuteList result: " + valuteList.get(j).getValue() + " рублей за " + valuteList.get(j).getNominal() + " " + valuteList.get(j).getName());
+    }
+
+        return valuteList;
+}
 
 
 }
--- app/src/main/java/com/example/currencydisplay/Valute.java
+++ app/src/main/java/com/example/currencydisplay/Valute.java
@@ -6,61 +6,71 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Valute {
 
-
-    private Date date;
-    private Date previousDate;
-    private URL previousURL;
-    private Time timestamp;
-    private String id;
-    private String numCode;
-    private String charCode;
+class Valute {
     private Integer nominal;
     private String name;
-    private Double value;
-    private Double previous;
-    private Map<String, Object> Valute = new HashMap<String, Object>();
+    private Long value;
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(Valute.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
-        sb.append("id");
-        sb.append('=');
-        sb.append(((this.id == null)?"<null>":this.id));
-        sb.append(',');
-        sb.append("numCode");
-        sb.append('=');
-        sb.append(((this.numCode == null)?"<null>":this.numCode));
-        sb.append(',');
-        sb.append("charCode");
-        sb.append('=');
-        sb.append(((this.charCode == null)?"<null>":this.charCode));
-        sb.append(',');
-        sb.append("nominal");
-        sb.append('=');
-        sb.append(((this.nominal == null)?"<null>":this.nominal));
-        sb.append(',');
-        sb.append("name");
-        sb.append('=');
-        sb.append(((this.name == null)?"<null>":this.name));
-        sb.append(',');
-        sb.append("value");
-        sb.append('=');
-        sb.append(((this.value == null)?"<null>":this.value));
-        sb.append(',');
-        sb.append("previous");
-        sb.append('=');
-        sb.append(((this.previous == null)?"<null>":this.previous));
-        sb.append(',');
-        if (sb.charAt((sb.length()- 1)) == ',') {
-            sb.setCharAt((sb.length()- 1), ']');
-        } else {
-            sb.append(']');
-        }
-        return sb.toString();
+
+    public Integer getNominal() {
+        return nominal;
     }
+
+    public void setNominal(Integer nominal) {
+        this.nominal = nominal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    public Valute(Integer nominal, String name, Long value) {
+        this.nominal = nominal;
+        this.name = name;
+        this.value = value;
+
+    }
+
+//    @Override
+//    public String toString() {
+//        StringBuilder sb = new StringBuilder();
+//        sb.append(Valute.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+//        sb.append("nominal");
+//        sb.append('=');
+//        sb.append(((this.nominal == null)?"<null>":this.nominal));
+//        sb.append(',');
+//        sb.append("name");
+//        sb.append('=');
+//        sb.append(((this.name == null)?"<null>":this.name));
+//        sb.append(',');
+//        sb.append("value");
+//        sb.append('=');
+//        sb.append(((this.value == null)?"<null>":this.value));
+//        sb.append(',');
+//        sb.append("previous");
+//        sb.append('=');
+//        sb.append(((this.previous == null)?"<null>":this.previous));
+//        sb.append(',');
+//        if (sb.charAt((sb.length()- 1)) == ',') {
+//            sb.setCharAt((sb.length()- 1), ']');
+//        } else {
+//            sb.append(']');
+//        }
+//        return sb.toString();
+//    }
 
 
 
